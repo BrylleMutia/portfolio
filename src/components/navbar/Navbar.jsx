07478313261a53558a84187fd6d82ea34fbaa777
Brylle Mutia <bryllemutia3@gmail.com>
2020-11-12 15:54:34 +0800

    added navbar responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,12 +7,12 @@ import cx from "classnames";
 const Navbar = () => {
     return (
         <nav className={navbar}>
-            <div className={cx(container, flex_row)} style={{ "--row-justify": "space-between" }}>
+            <div className={cx(container, flex_row, navbar__links)} style={{ "--row-justify": "space-between", "--row-wrap": "nowrap" }}>
                 <h3>Brylle Mutia.</h3>
                 <ul className={flex_row}>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Projects</a></li>
                     <li><a href="#">Skills</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
             </div>
         </nav>
